108d9f15779f41669cb6d7306af52f41bef8e9fb
Ahmad Sadati <a.sadati336@gmail.com>
2023-12-02 16:54:32 +0330

    added some routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/location', function () {
+    return view('location');
+});
+Route::get('/workouts', function () {
+    return view('workouts');
+});
